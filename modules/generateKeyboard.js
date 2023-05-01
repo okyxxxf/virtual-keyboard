4e37eac904c449e69d689321e2c1945f3b6a021b
Alexander <okyxxxf@gmail.com>
2023-05-01 22:30:51 +0300

    fix: rewrite wift eslint and rework class button

--- modules/generateKeyboard.js
+++ modules/generateKeyboard.js
@@ -1,36 +1,39 @@
 import Button from './Button';
-import keys from './keys';
+import {
+  caseDownEng, caseDownRu, caseUpEng, caseUpRu, capsEng, capsRu, shiftCapsEng, shiftCapsRu,
+} from './keys';
 
-export default function generateKeyboard(){
-    let selectors = [".firstStroke", ".secondStroke", ".thirdStroke", ".fourthStroke", ".fifthStroke"];
-    const board = document.querySelector('.keyboard');
+export default function generateKeyboard() {
+  const selectors = ['.firstStroke', '.secondStroke', '.thirdStroke', '.fourthStroke', '.fifthStroke'];
+  const board = document.querySelector('.keyboard');
 
-    const firstStroke = document.createElement('div');
-    firstStroke.className = "firstStroke";
+  const firstStroke = document.createElement('div');
+  firstStroke.className = 'firstStroke';
 
-    const secondStroke = document.createElement('div');
-    secondStroke.className = "secondStroke";
+  const secondStroke = document.createElement('div');
+  secondStroke.className = 'secondStroke';
 
-    const thirdStroke = document.createElement('div');
-    thirdStroke.className = "thirdStroke";
+  const thirdStroke = document.createElement('div');
+  thirdStroke.className = 'thirdStroke';
 
-    const fourthStroke = document.createElement('div');
-    fourthStroke.className = "fourthStroke";
+  const fourthStroke = document.createElement('div');
+  fourthStroke.className = 'fourthStroke';
 
-    const fifthStroke = document.createElement('div');
-    fifthStroke.className = "fifthStroke";
+  const fifthStroke = document.createElement('div');
+  fifthStroke.className = 'fifthStroke';
 
-    board.append(firstStroke);
-    board.append(secondStroke);
-    board.append(thirdStroke);
-    board.append(fourthStroke);
-    board.append(fifthStroke);
+  board.append(firstStroke);
+  board.append(secondStroke);
+  board.append(thirdStroke);
+  board.append(fourthStroke);
+  board.append(fifthStroke);
 
-    for(let i = 0; i < keys.length; i++){
-        for(let j = 0; j < keys[i].length; j++){
-            let button = new Button("50px", "50px", keys[i][j], "gray");
-            board.querySelector(selectors[i]).append(button.createButton());
-        }
+  for (let i = 0; i < caseDownEng.length; i += 1) {
+    for (let j = 0; j < caseDownEng[i].length; j += 1) {
+      const button = new Button('50px', '50px', caseDownRu[i][j], caseUpRu[i][j], capsRu[i][j], shiftCapsRu[i][j], caseDownEng[i][j], caseUpEng[i][j], capsEng[i][j], shiftCapsEng[i][j]);
+      const newButton = button.createButton();
+
+      board.querySelector(selectors[i]).append(newButton);
     }
-
+  }
 }
